8394845763c9fcc8ed8dcfbf4a7f2ad57d782fa4
faso-dev <jeromeonadja28@gmail.com>
2022-07-18 18:15:28 +0000

    feat(app): add routing and pages components

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,7 +1,15 @@
-export const TodoList = () => {
-  return (
-      <>
-          Todo list
-      </>
-  )
+import {TodoItem} from "./TodoItem";
+import {ITodoListProps} from "../../types/props";
+
+export const TodoList = ({todos}: ITodoListProps) => {
+    return (
+        <>
+            {todos.map((todo, index) => (
+                <TodoItem
+                    todo={todo}
+                    key={index}
+                />
+            ))}
+        </>
+    )
 }
